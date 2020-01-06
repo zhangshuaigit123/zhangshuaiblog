5187cf5ace9eb32c1fae4c36e997655910229877
zhangshuai <380194862@qq.com>
2020-01-06 09:42:03 +0800

    增加md

--- components/Author.js
+++ components/Author.js
@@ -7,7 +7,7 @@ const Author =()=>{
         <div className="author-div comm-box">
             <div> <Avatar size={100} src="http://blogimages.jspang.com/blogtouxiang1.jpg"  /></div>
             <div className="author-introduction">
-                光头程序员22，专注于WEB和移动前端开发。要录1000集免费前端视频的傻X。此地维权无门，此时无能为力，此心随波逐流。
+                光头程序员221，专注于WEB和移动前端开发。要录1000集免费前端视频的傻X。此地维权无门，此时无能为力，此心随波逐流。
                 <Divider>社交账号</Divider>
                 <Avatar size={28} icon="github" className="account"  />
                 <Avatar size={28} icon="qq"  className="account" />
